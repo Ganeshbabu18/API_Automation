97e46307620159f5f0fb8c3cac5ce0b092b8cdb6
Ganesh babu <95689460+Ganeshbabu18@users.noreply.github.com>
2023-06-06 14:28:00 +0530

    Update put_all_brand_2.py

--- put_all_brand.py
+++ put_all_brand.py
@@ -1,17 +1,25 @@
 import requests
 import pytest
 
-url = 'https://automationexercise.com/api/brandsList'
+@pytest.fixture
+def api_url():
+    return "https://automationexercise.com/api/brandsList"
 
-def put_all_brand():
-    updated_data = {
-        'brandName': 'India cement',
-        'brandStatus': 'Active'
-    }
-    response = requests.put(url, json=updated_data)
+def test_content_with_status(api_url):
+    # Test Case 1: Assert with Status
+    response = requests.put(api_url)
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+    response_content = response.json()
+    assert "brands" in response_content.keys(), "Response does not contain 'brands' key"
+    brands = response_content["brands"]
+    print(brands)
 
-    # assert the status code is 200 or 204
-    assert response.status_code in [200, 204]
-
-    # assert the response data is the same as the updated data
-    assert response.json() == updated_data
+def test_content_with_response_message(api_url):
+    # Test Case 2: Assert content with Response message
+    response = requests.put(api_url)
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+    response_content = response.json()
+    assert "message" in response_content.keys(), "Response does not contain 'message' key"
+    response_message = response_content["message"]
+    expected_message = "All brands list updated successfully"
+    assert response_message == expected_message, f"Unexpected response message: {response_message}"
